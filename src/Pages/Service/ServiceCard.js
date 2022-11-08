14bf36e0248fb446cd74017af3c575ccbabb71c4
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2022-11-08 18:28:21 +0600

    service card added

--- src/Pages/Service/ServiceCard.js
+++ src/Pages/Service/ServiceCard.js
@@ -1,28 +1,28 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-// const ServiceCard = ({ card }) => {
-//     const {title,img,price,description} = card
-//     return (
+const ServiceCard = ({ card }) => {
+    const {title,img,price,description} = card
+    return (
 
-//     <div className="card w-96 bg-base-100 shadow-xl">
-//   <figure className="px-10 pt-10">
-//     <img src={img} alt="Shoes" className="rounded-xl" />
-//   </figure>
-//   <div className="card-body items-center text-center">
-//         <h2 className="card-title">{ title}</h2>
-//         <h2 className="card-title">Price: ${ price}</h2>
-//         <p>{ description.slice(0,100)+'...'}</p>
-//         <div className="card-actions">
-//         <Link to={"/details"}>
-//         <button className="btn btn-primary">See Details</button>
-//          </Link>
-//     </div>
-//     </div>   
-//     </div>
+    <div className="card w-96 bg-base-100 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img src={img} alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+        <h2 className="card-title">{ title}</h2>
+        <h2 className="card-title">Price: ${ price}</h2>
+        <p>{ description.slice(0,100)+'...'}</p>
+        <div className="card-actions">
+        <Link to={"/details"}>
+        <button className="btn btn-primary">See Details</button>
+         </Link>
+    </div>
+    </div>   
+    </div>
             
             
-//     );
-// };
+    );
+};
 
-// export default ServiceCard;
+export default ServiceCard;
